2a2c3456d376100b38b641a17ff9e13c8c2ab689
Gavin Blanchard <168051577+Gavinn-B@users.noreply.github.com>
2026-03-28 12:19:06 -0400

    added post simplify request

--- server/routes/simplify.js
+++ server/routes/simplify.js
@@ -1,7 +1,40 @@
 import { Router } from "express";
 import upload from "../middleware/upload.js";
+import { extractTextFromPdf } from "../utils/pdfParser.js";
 import fs from "fs/promises";
 
 const router = Router();
 
+router.post("/simplify", upload.single('file'), async (req, res) => {
+    try{
+        let rawText = '';
+
+        if(req.file){
+            // PDF path 
+            rawText = await extractTextFromPdf(req.file.path);
+            await fs.unlink(req.file.path); // clean up tmp file
+        } else if (req.body.text){
+            // plain text path 
+            rawText = req.body.text;
+        } else {
+            return res.status(400).json({ error: 'No file or text provided' });
+        }
+        // TODO: call gemini api 
+        // const simplify = await 
+        res.json({ 
+            rawText,          // confirms pdf parsed correctly
+            summary: 'MOCK SUMMARY - Gemini not connected yet' 
+        });
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({ error: 'Failed to simplify PDF' });
+    };
+
+    
+    
+    
+});
+
+// TODO: implement gemini api call to get key terms and generate quiz
 export default router;
